01f367a36dd8c7b715ed5725f8d8ff1c8e85dc0b
Daniel Ferreira <danenoisturbera@gmail.com>
2018-07-16 20:20:56 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var record = DataLoader.Load(@"C:\Users\danielferreira\Documents\Edx\DataQueryingUsingLinqC\LinqApp");
+        }
+    }
+}
