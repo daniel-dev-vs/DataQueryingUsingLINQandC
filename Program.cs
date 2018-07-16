c4b1876508e551b8ef77d314f0245bfc996f5909
Daniel Ferreira <danenoisturbera@gmail.com>
2018-07-16 20:28:14 -0300

    First example linq fluent api style and linq query style

--- Program.cs
+++ Program.cs
@@ -9,6 +9,14 @@ namespace LinqApp
         static void Main(string[] args)
         {
             var record = DataLoader.Load(@"C:\Users\danielferreira\Documents\Edx\DataQueryingUsingLinqC\LinqApp");
+            var femaleTop10 = record.Where(r => r.Gender == Gender.Female && r.Rank <= 10);
+            var maleTop10 = from r in record where r.Gender == Gender.Male && r.Rank <= 10 select r;
+
+            foreach(var female in femaleTop10)
+                System.Console.WriteLine(female);
+
+            foreach(var male in maleTop10)
+                    System.Console.WriteLine(male);
         }
     }
 }
